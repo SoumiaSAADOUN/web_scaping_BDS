67a3a607d52aabd2b506c4c78f2ec4c40c9b85e9
SoumiaSAADOUN <es_saadoun@esi.dz>
2021-05-26 03:21:48 +0100

    test 5

--- src/test5.py
+++ src/test5.py
@@ -1,10 +1,37 @@
 import os.path
 import sys
 import json
+import logging
+import csv
+
 # opening json file
 # The file holds information about a category including the products
 f = open(os.path.join(sys.path[0],"data.json"))
-#  data is a Python dictionary
-data = json.loads(f.read())
+data = json.loads(f.read()) # data is a Python dictionary
 # .read() => pass the file contents (a string) to json.loads(), not the file object itself
-print (data)
+
+availableProds = open("available.csv","w",encoding="utf8", newline='') #file to save the available products
+hasHeader =False
+for i in data ["Bundles"]:
+    if (("Products" in i) and (i["Products"] )):
+        if (('Name' in i["Products"][0])):
+            productInfos= i["Products"][0]
+        else:
+            productInfos= i["Products"][1]   
+        try:
+            prodName= productInfos['Name'][0:30]
+            if (productInfos['IsAvailable']):
+                price= round(productInfos['Price'], 1)
+                if (os.stat("available.csv").st_size == 0 and not hasHeader) :
+                    writer = csv.DictWriter(availableProds, productInfos.keys())
+                    writer.writeheader()
+                    hasHeader =True
+                writer.writerow(productInfos)
+                print ("You can buy {} at our store at {} ".format(prodName,price))
+            else:
+                prodId= productInfos['Barcode']
+                print ("Product unavailable: {} {}".format(prodName,prodId))
+        except Exception:
+                logging.exception("Error occured: A clue of the product’s availability can’t be found")             
+           
+availableProds.close()
